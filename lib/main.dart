a2d76c5a83e8f8caa413fd24d4594fdad29e25e0
I-Love-yuigahama <jienweiyeong@gmail.com>
2024-10-25 14:40:59 +0800

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:ai_mental_health_chatbot/screens/chatbot/consts.dart';
 import 'package:ai_mental_health_chatbot/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 void main() {
-  runApp( const MaterialApp(
+  Gemini.init(apiKey: GEMINI_API_KEY,);
+  runApp(const MaterialApp(
     home: Home(),
   ));
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ai_mental_health_chatbot/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
